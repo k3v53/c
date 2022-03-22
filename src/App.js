5f87a06b180199f92f25b78e2ea56a3b8a39526d
k3v <25158905+k3v53@users.noreply.github.com>
2022-03-22 11:39:09 -0300

    Working version v0.0 (#1)
    * Removing stuff
    
    * Working version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
-
+import commands from './misc/commands'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [text, setText] = useState("Hello World")
+	const [history, setHistory] = useState([<p>{commands.welcome}</p>])
+	const searchInput = useRef(null);
+	function handleText(e) {
+		setText(e.target.value)
+	}
+	function handleSubmit(e){
+		e.preventDefault()
+		const closest_similar = Object.keys(commands).filter(e => e.includes(text))[0];
+		let fnReturn;
+		if(typeof commands[text] === "function"){
+			fnReturn = commands[text]()
+		}else if(typeof commands[closest_similar] === "function"){
+			fnReturn = commands[closest_similar]()
+		}
+		let el = <p>{'> ' + (commands[text] !== undefined? text : closest_similar || text) }</p>;
+		let res = <p>{'' + (
+			fnReturn || commands[text] || commands[closest_similar] ||
+			"Command not found, check the list from the bottom to know wich commands are available"
+		)}</p>;
+		setHistory([...history, el, res])
+		setText("")
+		searchInput.current.focus()
+
+	}
+	useEffect(()=>{
+		searchInput.current.focus();
+	 },[])
+
+	return (
+		<div className="App" onFocus={handleText}>
+			<div className='inputSection'>
+				{history}
+				<form onSubmit={handleSubmit}>
+					{'> '} <input className='inputTerminal' onInput={handleText} value={text}
+					ref={searchInput}
+					></input>
+				</form>
+				<p>{"Comandos Disponibles: " + Object.keys(commands).filter(e => e.includes(text)).join(", ")}</p>
+			</div>
+		</div>
+	);
 }
 
 export default App;
