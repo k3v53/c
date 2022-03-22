9df18a954171f588ceffc609aabf68c25fc29d7f
k3v <25158905+k3v53@users.noreply.github.com>
2022-03-22 15:46:25 -0300

    Tab autocompletion and more - Prevent executing a command when empty, instead prints an empty string

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import commands from './misc/commands'
 function App() {
-	const [text, setText] = useState("Hello World")
+	const [text, setText] = useState("hello world")
 	const [history, setHistory] = useState([<p>{commands.welcome}</p>])
+	const [textHistory, setTextHistory] = useState([])
+	const [historyPosition, setHistoryPosition] = useState(0)
 	const searchInput = useRef(null);
 	const scrollToInput = () => searchInput.current.scrollIntoView();
 	function handleText(e) {
@@ -11,24 +13,46 @@ function App() {
 	}
 	function handleSubmit(e){
 		e.preventDefault()
-		const closest_similar = Object.keys(commands).filter(e => e.includes(text))[0];
-		let fnReturn;
-		if(typeof commands[text] === "function"){
-			fnReturn = commands[text]()
-		}else if(typeof commands[closest_similar] === "function"){
-			fnReturn = commands[closest_similar]()
+		if(text){
+			const closest_similar = Object.keys(commands).filter(e => e.includes(text))[0];
+			let fnReturn;
+			if(typeof commands[text] === "function"){
+				fnReturn = commands[text]()
+			}else if(typeof commands[closest_similar] === "function"){
+				fnReturn = commands[closest_similar]()
+			}
+			let finalCommand = fnReturn || commands[text] || commands[closest_similar];
+			let el = <p>{'> ' + (commands[text] !== undefined? text : closest_similar || text) }</p>;
+			let res = <p>{'' + (
+				finalCommand ||
+				"Command not found, check the list from the bottom to know wich commands are available"
+			)}</p>;
+			setHistory([...history, el, res])
+			if(text === "clear"){
+				setHistory([]);
+			}
+			setText("");
+			scrollToInput();
+		}else{
+			setHistory([...history, <p>{">"}</p>])
 		}
-		let el = <p>{'> ' + (commands[text] !== undefined? text : closest_similar || text) }</p>;
-		let res = <p>{'' + (
-			fnReturn || commands[text] || commands[closest_similar] ||
-			"Command not found, check the list from the bottom to know wich commands are available"
-		)}</p>;
-		setHistory([...history, el, res])
-		if(text === "clear"){
-			setHistory([]);
+	}
+	function handleKeyDown(e){
+		switch (e.key) {
+			case "Tab":
+				e.preventDefault();
+				const closest_similar = Object.keys(commands).filter(e => e.includes(text))[0];
+				setText(closest_similar)
+			break;
+			case "ArrowUp":
+				// TODO: History on up
+			break;
+			case "ArrowDown":
+				// TODO: History on down
+			break;
+			default:
+				break;
 		}
-		setText("");
-		scrollToInput();
 	}
 	useEffect(()=>{
 		searchInput.current.focus();
@@ -36,11 +60,15 @@ function App() {
 	},[text, history])
 
 	return (
-		<div className="App" onFocus={handleText}>
+		<div className="App" >
 			<div className='inputSection'>
 				{history}
 				<form onSubmit={handleSubmit}>
-					{'> '} <input className='inputTerminal' onInput={handleText} value={text}
+					{`${historyPosition}/${textHistory.length}> `} <input
+					className='inputTerminal'
+					onKeyDown={handleKeyDown}
+					onInput={handleText}
+					value={text}
 					ref={searchInput}
 					></input>
 				</form>
