1169bacadf98e0a314a02a7bf38fa29634d9ab64
k3v <25158905+k3v53@users.noreply.github.com>
2022-03-29 01:08:04 -0300

    Removed commands and fixes - fixed padstart from que hora es - open a new tab on rick

--- src/misc/commands.js
+++ src/misc/commands.js
@@ -6,7 +6,7 @@ function answerFromTime(){
 }
 const getTimeString = () => {
 	const d = new Date();
-	return `son las ${String(d.getHours()).padStart(2)}:${String(d.getMinutes()).padStart(2)}hs`
+	return `son las ${String(d.getHours()).padStart(2,"0")}:${String(d.getMinutes()).padStart(2,"0")}hs`
 }
 const commands = {
 	"welcome": `Stupid terminal v0.0.3; type 'help' to be helped lol`,
@@ -18,24 +18,20 @@ const commands = {
 	"quien sos": "Soy una terminal, como estas? o quisiste decir 'quien es el creador'??",
 	"help": "No hay mucho que hacer aquí, soy una terminal que responde a comandos, los comandos se muestran debajo de la linea de input, si no hay nada en el input se muestran todos los comandos",
 	"ayuda": "No hay mucho que hacer aquí, soy una terminal que responde a comandos, los comandos se muestran debajo de la linea de input, si no hay nada en el input se muestran todos los comandos",
-	"info": "no se",
+	"toolbox": "Todavía no está disponible, te espero pronto en toolbox.kev.ar!!",
+	"info": "This is a simple website that simulates a command line, almost every command is in spanish but if somebody really find this site useful I can make the english versions of the same commands",
 	"r34": "No sé de que estás hablando",
 	"contacto": "contact@kev.ar",
 	"email": "contact@kev.ar",
-	"como puedo contactarte": "Pronto estaré lanzando un subdominio con mi contacto, espero verte pronto en contacto.kev.ar!!",
 	"como puedo contactarte?": "Pronto estaré lanzando un subdominio con mi contacto, espero verte pronto en contacto.kev.ar!!",
 	"puedes volar": "no",
 	"me gusta la pagina": "Muchas gracias!!! valoro tu opinión :D",
-	"esto está horrible": "Aunque parece fácil, te desafío a hacer esto en menos de 24 horas. Aaah ahora no parece tan fácil eh?",
 	// Preguntas del tipo "necesito"
 	"necesito comprar algo": "felicidades, estás en el sitio incorrecto",
-	"necesito dormir": answerFromTime,
 	"necesito dormir?": answerFromTime,
-	"deberia dormir": answerFromTime,
 	"deberia dormir?": answerFromTime,
 	// Preguntas de "que x"
 	"que hora es?": getTimeString,
-	"que productos vendes": "Pronto estaré lanzando una página que tendrá todo eso, probablemente será en portfolio.kev.ar",
 	"que productos vendes?": "Pronto estaré lanzando una página que tendrá todo eso, probablemente será en portfolio.kev.ar",
 	"que puedes hacer": "No sé, experimenta :D",
 	"para que sirve esto": "realmente no para mucho, simplemente devuelve valores y ya, piensa de esto como una especie de diccionario ;)",
@@ -47,7 +43,7 @@ const commands = {
 	"never gonna run around and desert you":"Never gonna make you cry",
 	"never gonna say goodbye": "Never gonna tell a lie and hurt you",
 	"rick": () => {
-		document.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+		window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", '_blank')
 		return "xd"
 	},
 	// Humor
